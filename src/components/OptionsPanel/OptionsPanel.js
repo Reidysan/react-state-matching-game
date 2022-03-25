c29b457fe67eceba0f06f604c752a8f353ab4d38
Callum Reid <callum.reid92@hotmail.com>
2022-03-25 11:48:52 +1100

    Completed module

--- src/components/OptionsPanel/OptionsPanel.js
+++ src/components/OptionsPanel/OptionsPanel.js
@@ -3,13 +3,15 @@ import React from 'react'
 import Button from '../Button'
 import TileSelector from '../TileSelector'
 
-const OptionsPanel = () => (
-  <div>
-    <TileSelector />
-    <Button />
-  </div>
-)
-  
-  
+function OptionsPanel(props) {
+  return (
+    <div>
+      <TileSelector numTiles={props.numTiles} />
+      <Button playing={props.playing} />
+    </div>
+  );
+}
+
+
 
 export default OptionsPanel
